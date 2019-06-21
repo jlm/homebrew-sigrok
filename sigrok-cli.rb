14cac498721bda551cc2773a55ad7d09ab3d8004
John Messenger <jmessenger@iee.org>
2019-06-21 13:25:35 +0100

    Works.

--- sigrok-cli.rb
+++ sigrok-cli.rb
@@ -1,26 +1,24 @@
-require 'formula'
-
 class SigrokCli < Formula
-  url 'http://sigrok.org/download/source/sigrok-cli/sigrok-cli-0.7.0.tar.gz'
-  homepage 'http://sigrok.org/'
-  head 'git://sigrok.org/sigrok-cli',:using => Class.new(GitDownloadStrategy) { def support_depth?; false; end }
-  sha256 '5669d968c2de3dfc6adfda76e83789b6ba76368407c832438cef5e7099a65e1c'
+  homepage "https://sigrok.org/"
+  url "https://sigrok.org/download/source/sigrok-cli/sigrok-cli-0.7.0.tar.gz"
+  sha256 "5669d968c2de3dfc6adfda76e83789b6ba76368407c832438cef5e7099a65e1c"
+  head "git://sigrok.org/sigrok-cli", :using => Class.new(GitDownloadStrategy) { def support_depth?; false; end }
 
-  depends_on 'rene-dev/sigrok/libsigrok'
-  depends_on 'rene-dev/sigrok/libsigrokdecode'
-  depends_on 'libserialport' => :optional
-  depends_on 'glib'
-  depends_on 'libtool' => :build
-  depends_on 'pkg-config' => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "jlm/sigrok/libsigrok"
+  depends_on "jlm/sigrok/libsigrokdecode"
+  depends_on "libserialport" => :optional
 
   def install
     ENV.delete "PYTHONPATH"
     if build.head?
       system "./autogen.sh"
     end
-    py_ver = Formula["python3"].pkg_version.to_s[0..2] # e.g "3.4"
+    py_ver = Formula["python"].pkg_version.to_s[0..2] # e.g "3.4"
     ENV.append_path "PKG_CONFIG_PATH", "#{HOMEBREW_PREFIX}/Frameworks/Python.framework/Versions/#{py_ver}/lib/pkgconfig/"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
