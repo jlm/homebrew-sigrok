c5e2f9c354120bd3568a723af653d2adc65895ec
John Messenger <jmessenger@iee.org>
2019-06-21 12:14:15 +0100

    Correct most of the audit messages

--- libsigrok.rb
+++ libsigrok.rb
@@ -1,23 +1,21 @@
-require 'formula'
-
 class Libsigrok < Formula
-  url 'http://sigrok.org/download/source/libsigrok/libsigrok-0.5.0.tar.gz'
-  homepage 'http://sigrok.org/'
-  head 'git://sigrok.org/libsigrok',:using => Class.new(GitDownloadStrategy) { def support_depth?; false; end }
-  sha256 '4c8c86779b880a5c419f6c77a08b1147021e5a19fa83b0f3b19da27463c9f3a4'
+  homepage "https://sigrok.org/"
+  url "https://sigrok.org/download/source/libsigrok/libsigrok-0.5.0.tar.gz"
+  sha256 "4c8c86779b880a5c419f6c77a08b1147021e5a19fa83b0f3b19da27463c9f3a4"
+  head "git://sigrok.org/libsigrok", :using => Class.new(GitDownloadStrategy) { def support_depth?; false; end }
 
-  depends_on 'libzip'
-  depends_on 'glib'
-  depends_on 'libserialport' => :optional
-  depends_on 'libftdi' => :optional
-  depends_on 'libusb'
-  depends_on 'libtool'
-  depends_on 'glibmm'
-  depends_on 'doxygen'
-  depends_on 'pkg-config' => :build
   depends_on "autoconf" => :build
   depends_on "autoconf-archive" => :build
   depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "doxygen"
+  depends_on "glib"
+  depends_on "glibmm"
+  depends_on "libtool"
+  depends_on "libusb"
+  depends_on "libzip"
+  depends_on "libftdi" => :optional
+  depends_on "libserialport" => :optional
 
   def install
     ENV.delete "PYTHONPATH"
@@ -26,7 +24,6 @@ class Libsigrok < Formula
     end
     system "./configure", "--prefix=#{prefix}", "--disable-java", "--disable-sysclk-lwla"
     system "make", "install"
-    system "ln", "-s", "/usr/local/share/", "#{prefix}/share"
+    system "ln", "-s", "/usr/local/share/", "#{share}"
   end
-
 end
